0aff8b0e1255f9cef61fb031c6c8bcd9efcbc4df
Teddy Guy <mediteddyguy@gmail.com>
2022-04-18 22:59:03 -0400

    affichage du niveau courrant a l'ecran

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
@@ -14,16 +16,27 @@ public class LevelManager : MonoBehaviour
     public Tirailleur tirailleur;
     public Brigand brigands;
     public Timer timerPrefab;
+    private Timer timer;
+    public TextMeshProUGUI textMesh;
     void Start()
     {
         Instantiate(player);
-        
-        var timer = Instantiate(timerPrefab);
+        currentLevel = 1;
+        timer = Instantiate(timerPrefab);
         timer.StartTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
+        DisplayCurrentLevel();
+        if (timer.currentTime > (15 * currentLevel/2)) {
+            timer.ResetTimer();
+            currentLevel++;
+        }
     }
+
+    public void DisplayCurrentLevel() {
+        textMesh.text = "Level " + currentLevel;
+}
 }
