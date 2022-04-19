4793eb1fca8ea6d47c344f7d0afc844819e1a5b6
Teddy Guy <mediteddyguy@gmail.com>
2022-04-18 22:36:32 -0400

    ajout d'un timer a l'ecran

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,10 +13,13 @@ public class LevelManager : MonoBehaviour
     public Asteroid mediumAsteroid, bigAsteroid;
     public Tirailleur tirailleur;
     public Brigand brigands;
-    public Timer timer;
+    public Timer timerPrefab;
     void Start()
     {
         Instantiate(player);
+        
+        var timer = Instantiate(timerPrefab);
+        timer.StartTimer();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,15 +6,39 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public TextMeshProUGUI text;
+    public float currentTime;
+    private bool isRunning;
+    public TextMeshProUGUI textMesh;
     void Start()
     {
-        text.text = "HAHAHA";
+        currentTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isRunning) {
+            
+            currentTime += Time.deltaTime;
+        }
+        DisplayTime();
+    }
+
+    public void StartTimer() { 
+        isRunning = true;
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = 0;
+    }
+    public void StopTimer() {
+        isRunning = false;
+    }
+
+    public void DisplayTime() {
+        var minutes = Mathf.Floor(currentTime / 60);
+        var seconds = Mathf.Floor(currentTime % 60);
+        textMesh.text = minutes + ":" + seconds;
     }
 }
